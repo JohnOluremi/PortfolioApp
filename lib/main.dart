5ace51130068f7454fb301589131a8c391d79978
JohnOluremi <johnakinteye@gmail.com>
2021-05-06 17:39:33 +0100

    PortfolioApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(PortfolioApp());
@@ -13,6 +14,18 @@ class PortfolioApp extends StatelessWidget {
         drawer: Drawer(
           child: Container(
             color: Colors.green[500],
+            child: Column(
+              children: [
+                Icon(
+                  Icons.person_outline_rounded,
+                  size: 50,
+                ),
+                Icon(
+                  Icons.settings,
+                  size: 50,
+                )
+              ],
+            ),
           ),
         ),
         appBar: AppBar(
@@ -28,6 +41,38 @@ class PortfolioApp extends StatelessWidget {
             'Portfolio',
             textDirection: TextDirection.ltr,
           ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.house_rounded,
+                  size: 35,
+                ),
+                label: '',
+                backgroundColor: Colors.green[400]),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search,
+                  size: 35,
+                ),
+                label: '',
+                backgroundColor: Colors.green[400]),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.email_outlined,
+                  size: 35,
+                ),
+                label: '',
+                backgroundColor: Colors.green[400]),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.notifications,
+                  size: 35,
+                ),
+                label: '',
+                backgroundColor: Colors.green[400]),
+          ],
         ),
       ),
     );
